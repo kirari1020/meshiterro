430e5a51b1b5d1b19ec5f158bd0c4135fc476611
kirari1020 <benkira1020@gmail.com>
2021-04-14 14:45:53 +0000

    meshiterro

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '2883ea090ba308721358236e7ffad76fc32e7b68e266594508f232d9b1df7702337f494d25a3dc3c6ac7f5c3451dfc3dd8d1cc465075df784a2ab5fb61868fed'
